796fa93623acc90965bdac9deaf769c25e507c10
Margaret A. <frailxup@gmail.com>
2025-08-27 17:47:23 -0400

    updated init and login modules for encrypted json file

--- init.py
+++ init.py
@@ -1,59 +1,68 @@
 import json
-from pathlib import Path
 import bcrypt
 import os
+from key import create_encryption_key, encrypt_vault
 
 def init():
   """
   Sets up the master password for the user and stores the hash in the vault.
   """
-  overwrite()
-  master_password = 'a'
-  confirmed_master_password = 'b'
-  while master_password != confirmed_master_password:
-    master_password = input("enter the master password: ")
-    confirmed_master_password = input("confirm your master password: ")
-    if master_password != confirmed_master_password:
-      print("error: passwords do not match")
-    else:
-      print("successfully confirmed master password.")
-  byte = master_password.encode('utf-8')
-  salt = bcrypt.gensalt()
-  hashed_password = bcrypt.hashpw(byte, salt)
-  create_vault(salt, hashed_password)
-  print(f"vault created. stored at {os.getcwd()}\'vault.json")
-  print("initialization successful")
+  if need_new_vault():
+    master_password = 'a'
+    confirmed_master_password = 'b'
+    while master_password != confirmed_master_password:
+      master_password = input("enter the master password: ")
+      confirmed_master_password = input("confirm your master password: ")
+      if master_password != confirmed_master_password:
+        print("error: passwords do not match.")
+      else:
+        print("successfully confirmed master password.")
+    byte = master_password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(byte, salt)
+    create_vault(salt, hashed_password)
+    print(f"vault created. stored at {os.getcwd()}\'vault.json")
+    print("initialization successful")
+
 def create_vault(salt, hashed_password):
   """
-  Creates a vault using an encrypted JSON value with the 
+  Creates a vault to store the salt and hashed password
   """
-  credentials = []
-  data = f'{{"salt": {salt}, "hashed_password": {hashed_password}, "credentials": {credentials}}}'
+  # decode the salt and hashed password to a string so JSON can serialize it
+  salt_str = salt.decode('utf-8')
+  hashed_password_str = hashed_password.decode('utf-8')
+  data = {"salt": salt_str, "hashed_password": hashed_password_str, "credentials": []}
+  # create json file
   f = open('vault.json', 'w')
   json.dump(data, f, indent=4)
+  f.close()
+  # encrypt json file
+  create_encryption_key()
+  encrypt_vault()
 
 def does_vault_exist():
   """
   Checks if the user has a stored vault
   """
-  a = Path('vault.json')
-  return a.exists()
+  a = os.path.exists('vault.json')
+  return a
 
-def overwrite():
+def need_new_vault():
   """
   Overwrites the vault if it exists
   """
-  if does_vault_exist():
+  if not does_vault_exist():
+    print("a vault can not be found on your pc.")
+    print("setting up new vault...")
+    return True
+  else:
     print("a vault currently exists on this PC.")
     print("would you like to set up a new vault?")
     new_vault_response = input("press 'y' for yes, or any other key for no: ").lower()
     if new_vault_response == 'y':
       os.remove('vault.json')
       print('vault removed.')
+      return True
     else:
-      print('No changes to vault made.')
-  else:
-    print("a vault can not be found on your pc")
-    print("setting up new vault")
-
-init()
+      print('no changes to vault made.')
+      return False
--- login.py
+++ login.py
@@ -1,4 +1,3 @@
-import base64
 import json
 import bcrypt
 
@@ -10,24 +9,17 @@ def login():
     current_attempt = 1
     logged_in = False
     while current_attempt <= MAX_ATTEMPT and not logged_in:
-        entered_password = input("enter your master password: ")
-        # get the data from the json file
-        with open('vault.json', 'r') as f:
-            data = json.load(f)
-        print(data)
-        # encode salt and master password to utf-8
-        salt = data["salt"].encode('utf-8')
-        hashed_master_password = data["hashed_password"].encode('utf-8')
-        # hash the entered password with the salt
-        entered_password_bytes = entered_password.encode('utf-8')
-        hashed_entered_password = bcrypt.hashpw(entered_password_bytes, salt)
-        if is_verified(hashed_entered_password, hashed_master_password):
-            print("successfully logged in.")
+        entered, master = get_hashed_passwords()
+        if is_verified(entered, master):
+            print("verification successful.")
             logged_in = True
+            return True
         else:
-            print("login unsuccessful.")
+            print("verification unsuccessful.")
             print(f"attempts left {MAX_ATTEMPT - current_attempt}")
             current_attempt += 1
+            logged_in = False
+    return False
 
 def is_verified(hashed_entered_password: bytes, hashed_master_password: bytes):
     """
@@ -35,5 +27,18 @@ def is_verified(hashed_entered_password: bytes, hashed_master_password: bytes):
     """
     return hashed_entered_password == hashed_master_password
 
+def get_hashed_passwords():
+    entered_password = input("enter your master password: ")
+    # get the data from the json file
+    with open('vault.json', 'r') as f:
+        data = json.load(f)
+    print(data)
+    # encode salt and master password to utf-8
+    salt = data["salt"].encode('utf-8')
+    hashed_master_password = data["hashed_password"].encode('utf-8')
+    # hash the entered password with the salt
+    entered_password_bytes = entered_password.encode('utf-8')
+    hashed_entered_password = bcrypt.hashpw(entered_password_bytes, salt)
+    return hashed_entered_password, hashed_master_password
 
 login()
