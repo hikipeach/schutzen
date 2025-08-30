a140dd6eacaf4741256641dd7378e7d60767e5e5
Alex A. <frailxup@gmail.com>
2025-08-30 16:29:18 -0400

    added clipboard feature: copies password and clears asynchronously after 30 seconds. testing is complete

--- src/utility.py
+++ src/utility.py
@@ -1,4 +1,7 @@
 import os
+import pyperclip3
+import time
+import asyncio
 
 def vault_exists():
   """
@@ -6,3 +9,21 @@ def vault_exists():
   """
   a = os.path.exists('vault.json')
   return a
+
+
+#ascynchronously clears clipboard after 30 seconds
+def copy_password(password):
+  """
+  Copies password to the clipboard
+  """
+  pyperclip3.copy(password)
+  print("your password has been copied to your clipboard and will be cleared after 30 seconds.")
+
+async def clear_password():
+  """
+  Clears clipboard after 30 seconds
+  """
+  # Needs to be asynchronous because this should not affect them using the program as a whole
+  await asyncio.sleep(30)
+  pyperclip3.clear()
+  return pyperclip3.paste()
