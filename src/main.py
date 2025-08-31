7c3253ed35116aaac636df5132a23fb9ffe540ae
Alex A. <frailxup@gmail.com>
2025-08-31 02:32:50 -0400

    added logout after 5 minutes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
+import asyncio
+import utility
+
 from src import delete, init, login
 import logging
 logging.basicConfig(filename='schutzen.log', encoding='utf-8', level=logging.INFO, format="%(levelname)s:%(message)s at %(asctime)s", datefmt='%m/%d/%Y %I:%M:%S %p')
 
 def main():
-    init.init()
+    asyncio.create_task(utility.logout())
+    #init.init()
     #login.login()
-    delete.delete()
+    #delete.delete()
 
 if __name__ == "__main__":
     main()
